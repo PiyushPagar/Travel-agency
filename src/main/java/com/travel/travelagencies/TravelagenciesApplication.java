e0d64936c9a3cbcf76c84e4a3d38832bddf4156d
Piyush Pagar <piyushpagar79@gmail.com>
2024-03-18 22:52:18 +0530

    travel agency code

---
+++ src/main/java/com/travel/travelagencies/TravelagenciesApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelagencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelagenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelagenciesApplication.class, args);
+	}
+
+}
